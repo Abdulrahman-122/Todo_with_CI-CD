d9e0524185f4f62d373ebf672b69f86cb00a9a3b
Abdulrahman Qasim <abdulrahman11510.qasim@gmail.com>
2026-06-23 19:41:20 +0300

    check pytest-cov

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,10 +1,37 @@
 from fastapi.testclient import TestClient
 from main.app import app
 import uuid
+import pytest
+
 client = TestClient(app)
 
-email=f"john{uuid.uuid4()}@demo.com"
+
+@pytest.fixture
+def test_user():
+    email = f"john{uuid.uuid4()}@demo.com"
+    password = "TTYSrecrets"
+    username = "John"
+
+    # register user
+    res = client.post(
+        "/auth/register",
+        json={
+            "username": username,
+            "email": email,
+            "password": password,
+        },
+    )
+    assert res.status_code == 201
+
+    return {
+        "email": email,
+        "password": password
+    }
+
+
 def test_register():
+    email = f"john{uuid.uuid4()}@demo.com"
+
     res = client.post(
         "/auth/register",
         json={
@@ -13,14 +40,21 @@ def test_register():
             "password": "TTYSrecrets",
         },
     )
+
     assert res.status_code == 201
 
 
-def test_login():
+def test_login(test_user):
     res = client.post(
-        "/auth/login", json={"email": email, "password": "TTYSrecrets"}
+        "/auth/login",
+        json={
+            "email": test_user["email"],
+            "password": test_user["password"],
+        },
     )
+
     assert res.status_code == 200
     data = res.json()
+
     assert "access_token" in data
     assert data["token_type"] == "bearer"
