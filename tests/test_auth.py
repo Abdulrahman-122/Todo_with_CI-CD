95134101535f9097fc6cca66c00c7658f127d0d1
Abdulrahman Qasim <abdulrahman11510.qasim@gmail.com>
2026-06-23 19:05:40 +0300

    Check coverage_workflow

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,8 +8,8 @@ def test_register():
     res = client.post(
         "/auth/register",
         json={
-            "username": "John6",
-            "email": "john6@demo.com",
+            "username": "John0",
+            "email": "john0@demo.com",
             "password": "TTYSrecrets",
         },
     )
@@ -18,7 +18,7 @@ def test_register():
 
 def test_login():
     res = client.post(
-        "/auth/login", json={"email": "john6@demo.com", "password": "TTYSrecrets"}
+        "/auth/login", json={"email": "john0@demo.com", "password": "TTYSrecrets"}
     )
     assert res.status_code == 200
     data = res.json()
