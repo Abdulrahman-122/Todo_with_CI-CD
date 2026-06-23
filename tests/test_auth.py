83dc63ff5211cda93c177aa1ed1706bb5237e209
Abdulrahman Qasim <abdulrahman11510.qasim@gmail.com>
2026-06-23 19:25:55 +0300

    check pytest-cov

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,15 +1,15 @@
 from fastapi.testclient import TestClient
 from main.app import app
-
+import uuid
 client = TestClient(app)
 
-
+email=f"john{uuid.uuid4()}@demo.com"
 def test_register():
     res = client.post(
         "/auth/register",
         json={
-            "username": "John12",
-            "email": "john12@demo.com",
+            "username": "John",
+            "email": email,
             "password": "TTYSrecrets",
         },
     )
@@ -18,7 +18,7 @@ def test_register():
 
 def test_login():
     res = client.post(
-        "/auth/login", json={"email": "john12@demo.com", "password": "TTYSrecrets"}
+        "/auth/login", json={"email": email, "password": "TTYSrecrets"}
     )
     assert res.status_code == 200
     data = res.json()
