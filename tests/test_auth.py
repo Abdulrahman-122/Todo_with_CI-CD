16f3c3732b95e0c24806776c734c2f609f1ad189
Abdulrahman Qasim <abdulrahman11510.qasim@gmail.com>
2026-06-23 19:45:03 +0300

    check pytest-cov

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,60 +1,26 @@
 from fastapi.testclient import TestClient
 from main.app import app
 import uuid
-import pytest
-
 client = TestClient(app)
 
 
-@pytest.fixture
-def test_user():
-    email = f"john{uuid.uuid4()}@demo.com"
-    password = "TTYSrecrets"
-    username = "John"
-
-    # register user
-    res = client.post(
-        "/auth/register",
-        json={
-            "username": username,
-            "email": email,
-            "password": password,
-        },
-    )
-    assert res.status_code == 201
-
-    return {
-        "email": email,
-        "password": password
-    }
-
-
 def test_register():
-    email = f"john{uuid.uuid4()}@demo.com"
-
     res = client.post(
         "/auth/register",
         json={
             "username": "John",
-            "email": email,
+            "email": "john2@demo.com",
             "password": "TTYSrecrets",
         },
     )
-
     assert res.status_code == 201
 
 
-def test_login(test_user):
+def test_login():
     res = client.post(
-        "/auth/login",
-        json={
-            "email": test_user["email"],
-            "password": test_user["password"],
-        },
+        "/auth/login", json={"email": "john2@demo.com", "password": "TTYSrecrets"}
     )
-
     assert res.status_code == 200
     data = res.json()
-
     assert "access_token" in data
     assert data["token_type"] == "bearer"
